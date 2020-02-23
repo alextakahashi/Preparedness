6ee8d53960290ef086008219b6b0ada9e4748baa
Sebastian Wild <sebastianrwild@gmail.com>
2020-02-23 10:40:28 -0800

    Initial Commit

---
+++ Preparedness/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Preparedness
+//
+//  Created by Sebastian Wild on 2/23/20.
+//  Copyright © 2020 Sebastian Wild. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
