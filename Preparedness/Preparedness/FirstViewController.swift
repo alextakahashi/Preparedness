945f26c2db74424211607b87d045ea7fffb07df8
SebasWild <sebastianrwild@gmail.com>
2020-04-12 12:58:58 -0700

    #31 Lower deployment target
    Now targeting iOS11 as we would like to support older devices as well.
    Created using the "tabbed app" template, so we're starting off with a tab bar

---
+++ Preparedness/Preparedness/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Preparedness
+//
+//  Created by Sebastian Wild on 4/12/20.
+//  Copyright © 2020 CCDC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
