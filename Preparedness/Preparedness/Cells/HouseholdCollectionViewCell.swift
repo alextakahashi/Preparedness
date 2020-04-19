e1c5068a66948ea519ca9997bdf5c5455c801ba6
alextakahashi <alextakahashi@berkeley.edu>
2020-04-18 23:01:42 -0700

    setup IGListKit work

---
+++ Preparedness/Preparedness/Cells/HouseholdCollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  HouseholdCollectionViewCell.swift
+//  Preparedness
+//
+//  Created by Rosa Chen on 4/18/20.
+//  Copyright © 2020 CCDC. All rights reserved.
+//
+
+import Foundation
---
+++ Preparedness/Preparedness/SectionControllers/HouseholdSectionController.swift
@@ -0,0 +1,11 @@
+import IGListKit
+
+class LabelSectionController: ListSectionController {
+  override func sizeForItem(at index: Int) -> CGSize {
+    return CGSize(width: collectionContext!.containerSize.width, height: 55)
+  }
+
+  override func cellForItem(at index: Int) -> UICollectionViewCell {
+    return collectionContext!.dequeueReusableCell(of: MyCell.self, for: self, at: index)
+  }
+}
